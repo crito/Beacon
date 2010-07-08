2bda673a5148b90f83a3fe2b2b21f92a1d0c86b7
Christo Buschek <crito@30loops.net>
2010-07-08 22:57:44 +0200

    The LocationListener sends now a HTTP PUT request.

--- src/net/thirtyloops/hitchhiking/beacon/Beacon.java
+++ src/net/thirtyloops/hitchhiking/beacon/Beacon.java
@@ -1,22 +1,31 @@
 package net.thirtyloops.hitchhiking.beacon;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.util.Log;
 
 public class Beacon extends Activity {
+	private final String TAG = "Beacon";
+	
 	private LocationManager lm;
 	private LocationListener locationListener;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	Log.v(TAG, TAG + " started");
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
+        setContentView(R.layout.main);
         
         //---- Obtain GPS location
         lm = (LocationManager)
@@ -26,19 +35,37 @@ public class Beacon extends Activity {
         
         lm.requestLocationUpdates(
         		LocationManager.GPS_PROVIDER,
-        		0,
-        		0,
+        		100,
+        		10,
         		locationListener);
     }
     
     private class MyLocationListener implements LocationListener
     {
+    	private HttpURLConnection httpCon = null;
+    	private URL url = null;
     	public void onLocationChanged(Location loc) {
     		if (loc != null) {
-    			Toast.makeText(getBaseContext(), 
-    					"Location changed: Lat: " + loc.getLatitude() + 
-    					" Lng: " + loc.getLongitude(), 
-    					Toast.LENGTH_SHORT).show();
+    			// Generate URL instance
+				try {
+					url = new URL("http://www.mariazendre.org/hitchhiking/position/");
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				// Do a HTTP PUT request to post the GPS location to url
+				try {
+					httpCon = (HttpURLConnection) url.openConnection();
+	    			httpCon.setDoOutput(true);
+	    			httpCon.setRequestMethod("PUT");
+	    			OutputStreamWriter out = new OutputStreamWriter(httpCon.getOutputStream());
+	    			out.write("Lat=" + loc.getLatitude() + ";Long=" + loc.getLongitude());
+	    			out.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
     		}
     	}
     	public void onProviderDisabled(String provider) {
