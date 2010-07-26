820d1df68de996a69e59d1a2d11cd49d2a845e5a
Christo Buschek <crito@cryptodrunks.net>
2010-07-26 18:06:20 +0200

    GPS location gets turned into a compact JSON string and send as a PUT request to a specified URL.

--- src/net/thirtyloops/hitchhiking/beacon/Beacon.java
+++ src/net/thirtyloops/hitchhiking/beacon/Beacon.java
@@ -1,11 +1,5 @@
 package net.thirtyloops.hitchhiking.beacon;
 
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
@@ -13,6 +7,19 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+import java.io.IOException;
+import java.lang.String;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.json.JSONObject;
+import org.json.JSONException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.entity.ByteArrayEntity;
+
 
 public class Beacon extends Activity {
 	private final String TAG = "Beacon";
@@ -20,6 +27,12 @@ public class Beacon extends Activity {
 	private LocationManager lm;
 	private LocationListener locationListener;
 	
+	//private OnClickListener mCorkyListener = new OnClickListener() {
+	//    public void onClick(View v) {
+	      // do something when the button is clicked
+	//    }
+	//};
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,7 +40,7 @@ public class Beacon extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        //---- Obtain GPS location
+        // Obtain GPS location updates and connect it to a callback class locationListener.
         lm = (LocationManager)
         	getSystemService(Context.LOCATION_SERVICE);
         
@@ -35,37 +48,60 @@ public class Beacon extends Activity {
         
         lm.requestLocationUpdates(
         		LocationManager.GPS_PROVIDER,
-        		100,
+        		2000,
         		10,
         		locationListener);
     }
     
     private class MyLocationListener implements LocationListener
     {
-    	private HttpURLConnection httpCon = null;
-    	private URL url = null;
+    	
+    	private URI uri = null;	// URI to make the HTTP PUT request to.
+    	
     	public void onLocationChanged(Location loc) {
     		if (loc != null) {
+    			// Used for the user notifications, Toast
+    			Context context = getApplicationContext();
+    			int duration = Toast.LENGTH_SHORT;
+
     			// Generate URL instance
 				try {
-					url = new URL("http://www.mariazendre.org/hitchhiking/position/");
-				} catch (MalformedURLException e) {
+					uri = new URI("http://www.mariazendre.org/hitchhiking/position/");
+				} catch (URISyntaxException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				
-				// Do a HTTP PUT request to post the GPS location to url
+				// Create the json string to PUT to the server
+				JSONObject data = new JSONObject();
 				try {
-					httpCon = (HttpURLConnection) url.openConnection();
-	    			httpCon.setDoOutput(true);
-	    			httpCon.setRequestMethod("PUT");
-	    			OutputStreamWriter out = new OutputStreamWriter(httpCon.getOutputStream());
-	    			out.write("Lat=" + loc.getLatitude() + ";Long=" + loc.getLongitude());
-	    			out.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					data.put("lat", loc.getLatitude());
+					data.put("lon", loc.getLongitude());
+				} catch (JSONException e) {
+					Toast toast = Toast.makeText(context, "JSON creation failed.", duration);
+					toast.show();
 				}
+
+			    try {
+			    	// Do a HTTP PUT request to post the GPS location to url
+					HttpClient httpclient = new DefaultHttpClient();
+					
+					// Prepare a request object
+				    HttpPut httpput = new HttpPut(uri);
+				    httpput.setEntity(new ByteArrayEntity(data.toString().getBytes()));
+					
+				    // Execute the request
+				    httpclient.execute(httpput);
+
+			    } catch (ClientProtocolException e) {
+		            // TODO Auto-generated catch block
+		            e.printStackTrace();
+				} catch (IOException e) {
+					Toast toast = Toast.makeText(context, "Put failed", duration);
+					toast.show();
+				}
+				Toast toast = Toast.makeText(context, data.toString(), duration);
+    			toast.show();
     		}
     	}
     	public void onProviderDisabled(String provider) {
